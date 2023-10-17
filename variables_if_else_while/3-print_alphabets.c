ee093c514b06f2b1fa10899fd249fc05a1d7758c
ken2201 <ndayishimiyeken22@gmail.com>
2023-10-17 00:17:11 -0700

    variable if and co

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print lower and upper case alphabet
+ *
+ * return : always (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
