dea8d4a529e65bab41e66ab7095397cdf47125ef
ken2201 <ndayishimiyeken22@gmail.com>
2023-10-17 00:41:23 -0700

    variable if and co

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print lower case except q and e
+ *
+ *Return: always (success)
+ **/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= z; c++)
+{
+if (c != 'e' && c != 'q')
+{
+putchar(c);
+}
+}
+putchar('\n');
+return (0);
+}
