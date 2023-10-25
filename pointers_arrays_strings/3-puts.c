0eb20c13c25080f829ad9cb1b9bd8de14f60bdcb
ken2201 <ndayishimiyeken22@gmail.com>
2023-10-24 17:01:39 -0700

    pointers and arrays

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _puts - return the string with a new line 
+ * _puts - return the string with a new line
  * @str: the string
  */
 void _puts(char *str)
 {
-	printf("%c\n",str)
+	printf("%c\n", str)
 }
