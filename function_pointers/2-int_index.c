bb989c15e5f0d148a687af943fd120edf4d3f5d3
ken2201 <ndayishimiyeken22@gmail.com>
2023-11-17 10:15:13 -0800

    funct-pointers

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,30 +1,23 @@
-#include "function_pointers.h"
+#include <stddef.h>
 
 /**
-  * int_index - ...
-  * @array: ...
-  * @size: ...
-  * @cmp: ...
-  *
-  * Return: ...
-  */
+ * int_index - a func that check an integ
+ * @array: an integ array of size
+ * @size: the size of array
+ * @cmp: compare func to be used
+ * Return: -1 on error
+ */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i = 0;
+	int i;
 
-	if (size > 0)
-	{
-		if (array != NULL && cmp != NULL)
-		{
-			while (i < size)
-			{
-				if (cmp(array[i]))
-					return (i);
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
 
-				i++;
-			}
-		}
-	}
+	for (i = 0; i < size; i++)
+		if (cmp(array[i]))
+			return (i);
 
 	return (-1);
 }
