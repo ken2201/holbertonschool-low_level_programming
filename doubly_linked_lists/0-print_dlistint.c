495cbaaf34097234af1554fa3f2980c1b42dbb48
ken2201 <ndayishimiyeken22@gmail.com>
2023-12-01 11:41:41 -0800

    d-l-l

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Print all elements of a doubly linked list
+ * @h: address of linked list
+ * Return: number of elements
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t d;
+
+	for (d = 0; h != NULL; h = h->next, d++)
+		printf("%d\n", h->n);
+	return (d);
+}
---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - function that adds a new node at the end of the list
+ * @head: pointer to the first element
+ * @n: the integer to add
+ * Return: the address of the new element, NULL if it failed
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *tmp;
+
+	if (head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		new->next = NULL;
+		new->prev = NULL;
+		return (new);
+	}
+
+	tmp = *head;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+	new->next = NULL;
+	new->prev = tmp;
+	tmp->next = new;
+
+	return (new);
+}
